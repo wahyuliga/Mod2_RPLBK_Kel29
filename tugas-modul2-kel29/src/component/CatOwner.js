0fda9d19d6a45f863758f27646f9bac94df71b6d
wahyuliga <wahyuligas@yahoo.com>
2021-09-11 23:24:56 +0700

    Adding style, image
    Styling using bootstrap

--- tugas-modul2-kel29/src/component/CatOwner.js
+++ tugas-modul2-kel29/src/component/CatOwner.js
@@ -3,15 +3,27 @@ import React from "react";
 export default function CatOwner(props){
     return(
         <>
-            <h3>Nama: {props.nama}</h3>
-            <h4>Asal: {props.asal}</h4>
+            <h3 style={{marginLeft: "25px"}}><span class="badge bg-dark">Kocheng.com</span></h3>
+            <h4 style={{marginLeft: "25px"}}>Informasi Pemilik</h4>
+            <ul className="list-group" style={{width: "20rem", margin: "0 0 25px 25px"}}>
+                <li className="list-group-item">
+                    <h6>Nama Pemilik: {props.nama}</h6>
+                </li>
+                <li className="list-group-item">
+                    <h6>Asal: {props.asal}</h6>
+                </li>
+            </ul>
             {props.data.map((value) => (
                 <>
-                    <h4>Nama: {value.nama}</h4>
-                    <h4>Jenis: {value.jenis}</h4>
-                    <img src={value.gambar} />
+                    <div className="card shadow" style={{width: "20rem", margin: "auto", float: "left", marginLeft: "25px"}}>
+                        <img src={value.gambar} className="card-img-top" alt={value.jenis} />
+                        <div className="card-body">
+                            <h5 className="card-title">{value.nama}</h5>
+                            <p>{value.jenis}</p>
+                        </div>
+                    </div>
                 </>
-            ))}
+            ))}  
         </>
     );
 }
--- tugas-modul2-kel29/src/index.js
+++ tugas-modul2-kel29/src/index.js
@@ -4,13 +4,13 @@ import './index.css';
 import CatOwner from './component/CatOwner';
 
 const data = [
-  {nama: "Miku", jenis:"British Shorthair", gambar: "https://cdn.onemars.net/sites/whiskas_id_98_2/image/editor/photo-3_1581586570401.jpg"}, 
-  {nama: "Madu", jenis:"Scottish Fold", gambar: "https://cdn.onemars.net/sites/whiskas_id_98_2/image/editor/photo-3_1581593291215.jpg"}
+  {nama: "Britsy", jenis:"British Shorthair", gambar: "https://cdn.onemars.net/sites/whiskas_id_98_2/image/editor/photo-3_1581586570401.jpg"}, 
+  {nama: "Scoty", jenis:"Scottish Fold", gambar: "https://cdn.onemars.net/sites/whiskas_id_98_2/image/editor/photo-3_1581593291215.jpg"}
 ] 
 
 ReactDOM.render(
   <React.StrictMode>
-    <CatOwner nama="Liga" asal="Semarang" data={data} />
+    <CatOwner nama="Wahyu Liga" asal="Semarang" data={data} />
   </React.StrictMode>,
   document.getElementById('root')
 );
